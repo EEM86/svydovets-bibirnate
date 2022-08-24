4a0d63da83f18f0eae2d54df76d83011c2dec4ab
khshanovskyi <khshanovskyi@gmail.com>
2022-08-24 12:20:30 +0300

    [BIB-29] : Add tests for CommandUtil

---
+++ src/test/java/com/svydovets/bibirnate/cache/command/CacheConstant.java
@@ -0,0 +1,8 @@
+package com.svydovets.bibirnate.cache.command;
+
+public class CacheConstant {
+    public static final Long ONE = 1L;
+    public static final String QUERY = "SELECT * FROM products";
+    public static final String SELECT = "SELECT ";
+
+}
